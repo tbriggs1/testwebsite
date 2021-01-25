f134f8eda87dba86a6bfaeadcd84af758dd8c814
tbriggs1 <tombriggs93@protonmail.com>
2021-01-25 18:50:11 +0000

    burgermenu

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
 var burgerMenu = document.getElementById('burgerIcon');
 var burgerContent = document.getElementById('navMenu');
 var burgerIcons = document.getElementsByClassName('burgerButtons');
-var burgerClose = document.getElementById('burgerIcon:after')
-
-
 
 burgerMenu.addEventListener('click', function(){
-    burgerContent.style.visibility = 'visible';
-    burgerMenu.classList.add('burgerClose');
-    burgerMenu.removeAttribute('burgerIcon');
-    burgerIcons[0].classList.add('changeB1');
-    burgerIcons[1].classList.add('changeB2');
-    burgerIcons[2].classList.add('changeB3');
+    let getStyle = window.getComputedStyle(burgerContent);
+    if(getStyle.visibility === 'hidden'){
+        burgerContent.style.visibility = 'visible';
+        burgerIcons[0].classList.add('changeB1');
+        burgerIcons[1].classList.add('changeB2');
+        burgerIcons[2].classList.add('changeB3');
+    } else {
+        burgerContent.style.visibility = 'hidden';
+        burgerIcons[0].classList.remove('changeB1');
+        burgerIcons[1].classList.remove('changeB2');
+        burgerIcons[2].classList.remove('changeB3');
+    }
 })
 
 
-burgerClose.addEventListener('click', function(){
-    burgerContent.style.visibility = 'hidden';
-    burgerMenu.classList.remove('burgerClose');
-    burgerMenu.id = 'burgerIcon';
-    burgerIcons[0].classList.remove('changeB1');
-    burgerIcons[1].classList.remove('changeB2');
-    burgerIcons[2].classList.remove('changeB3');
-})
+
+
 
 
 
